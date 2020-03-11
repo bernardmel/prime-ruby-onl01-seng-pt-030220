05ecd0a0fa10cacf5b709018a0377f0ad24ebe7c
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-11 23:30:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,9 @@ def prime?(number)
    n = 2
    if number > 1
      range = (n..number-1).to_a
-     range.none? do |num_to_test|
+     range.none? do |test_num|
 
-       number % num_to_test == 0
+       number % test_num == 0
      end
    else
      false
