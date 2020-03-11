a230c875a9a66ff997e9e0fdf67a52ef18e7c5de
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-11 23:37:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(number)
+   n = 2
+   if number > 1
+     range = (n..number-1).to_a
+     range.none? do |test_num|
+
+       number % test_num == 0
+     end
+   else
+     false
+   end
+
+end
